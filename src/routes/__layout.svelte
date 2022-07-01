378ecd6f81cd2d3c59ef22697a464263d587fc8b
Sakulbuth Ekvittayaniphon <sakulbuth@gmail.com>
2022-07-01 17:38:24 +0700

    empty repo with tailwind and daisyui

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+   import "../app.css";
+ </script>
+ 
+ <main class="w-screen text-center">
+   <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script lang=ts>
+   import logo from '$lib/images/logo.png'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img class="h-20 aspect-square" src={logo} alt="logo">
+
+<div class="text-primary">hey</div>
+<div class='text-secondary'>second</div>

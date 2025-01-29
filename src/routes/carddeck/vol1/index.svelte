042d78b78b0ec01a5feec518723ea9c39be613a6
Sakulbuth Ekvittayaniphon <sakulbuth@gmail.com>
2025-01-29 17:28:38 +0700

    fix img extension

--- src/routes/carddeck/vol1/index.svelte
+++ src/routes/carddeck/vol1/index.svelte
@@ -9,12 +9,12 @@
 <h1><a href="./">Puzzle Card Deck Vol.1</a></h1>
 
 <div class="flex flex-col justify-left text-left">
-   <img src="https://raw.githubusercontent.com/lemononmars/codebreaker/main/src/lib/images/event/product_carddeck1_promo" onerror='this.style.display = "none"' class="aspect-auto object-contain" alt="img">
+   <img src="https://raw.githubusercontent.com/lemononmars/codebreaker/main/src/lib/images/event/product_carddeck1_promo.jpg" onerror='this.style.display = "none"' class="aspect-auto object-contain" alt="img"><br>
 
    <p>Puzzle Card Deck Vol.1 พร้อมจำหน่ายแล้วในราคาเพียง 150 บาท
-   ค่าส่งเพียง 30 บาทเท่านั้น (+10 บาทต่อชุด ในกรณีสั่งหลายชุดในออเดอร์เดียวกัน)</p>
+   ค่าส่งเพียง 30 บาทเท่านั้น (+10 บาทต่อชุด ในกรณีสั่งหลายชุดในออเดอร์เดียวกัน)</p><br>
 
-   <p>การ์ดเกมไขปริศนาจากเพจ Code Breaker Thailand ที่ขนปริศนาจุก ๆ มาให้คุณได้ไขถึง 17 ปริศนาสุดคลาสสิกบนเพจ และพิเศษกับ 1 Meta Puzzle ใหม่ในชุดนี้โดยเฉพาะ</p>
+   <p>การ์ดเกมไขปริศนาจากเพจ Code Breaker Thailand ที่ขนปริศนาจุก ๆ มาให้คุณได้ไขถึง 17 ปริศนาสุดคลาสสิกบนเพจ และพิเศษกับ 1 Meta Puzzle ใหม่ในชุดนี้โดยเฉพาะ</p><br>
 
    <p>สนใจติดต่อทาง inbox ของเพจ <a href="http://m.me/codebreakerTH" target="_blank">Code Breaker Thailand</a> ได้แล้ววันนี้</p>
 </div>

bb3901fc903c22102b05afa546d3b5025a742c1f
Sakulbuth Ekvittayaniphon <sakulbuth@gmail.com>
2025-03-29 16:03:34 +0700

    remove unused api folders

--- src/lib/interfaces/database.ts
+++
@@ -1,7 +0,0 @@
-export interface Leaderboard {
-   name: string,
-   puzzle_type: string,
-   puzzle_id: number,
-   score: number,
-   created_at: string
-}
--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -1,3 +1,2 @@
 export * from './content'
-export * from './database'
 export * from './jeopardy'
